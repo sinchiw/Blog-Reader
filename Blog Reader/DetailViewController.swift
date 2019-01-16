609142c8d77f9f0ecbfe0c9521e844bbe19d5889
Wilmer sinchi <wilmersinchi@gmail.com>
2019-01-16 18:17:36 -0500

    Initial Commit

---
+++ Blog Reader/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Blog Reader
+//
+//  Created by Wilmer sinchi on 1/16/19.
+//  Copyright © 2019 Wilmer sinchi. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.timestamp!.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: Event? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
